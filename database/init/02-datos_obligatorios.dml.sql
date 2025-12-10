fb4dd2f273cd97b7f81cf0d86bd94b1a03cb8a48
Ju4nD1ego <juan.ledezma@correounivalle.edu.co>
2025-12-09 20:43:51 -0500

    Add: Inserts obligatorios de Institucion

--- database/init/02-datos_obligatorios.dml.sql
+++ database/init/02-datos_obligatorios.dml.sql
@@ -18,3 +18,11 @@ INSERT INTO Variable (tipo, nombre, unidad, descripcion) VALUES
 ('METEOROLOGICA', 'Dirección del Viento', 'grados', 'Dirección de procedencia del viento'),
 ('METEOROLOGICA', 'Presión Atmosférica', 'hPa', 'Presión atmosférica'),
 ('METEOROLOGICA', 'Precipitación', 'mm', 'Cantidad de lluvia acumulada');
+
+-- 1. Usuario Administrador de la Institución
+INSERT INTO usuario (nombre, correo_electronico, contraseña, tipo_usuario, fecha_registro)
+VALUES ('Admin Dagma', 'admin@dagma.gov.co', '1234', 'INSTITUCION', NOW());
+
+-- 2. Institución asociada (Usando subconsulta para obtener el ID automáticamente)
+INSERT INTO institucion (nombre, logo, direccion, estado, id_usuario, fecha_registro)
+VALUES ('DAGMA Oficial', 'logo.png', 'Av 6N', 'ACTIVA', (SELECT id_usuario FROM usuario WHERE correo_electronico='admin@dagma.gov.co' LIMIT 1), NOW());
